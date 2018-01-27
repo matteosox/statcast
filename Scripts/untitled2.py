4b709fcd26532d9d609e7f6d7086660d8f35de8a
Matt Fay <mattfay@Matts-MacBook.local>
2018-01-27 12:46:24 -0800

    Updating some scripts

--- Scripts/untitled2.py
+++ Scripts/untitled2.py
@@ -1,16 +1,13 @@
 # %% Imports
 
-import numpy as np
-
-from statsmodels import api as sm
 from sklearn.model_selection import StratifiedKFold
 from sklearn.metrics import log_loss
+from sklearn.base import clone
 from sklearn.preprocessing import LabelBinarizer
 
 from statcast.bip import Bip
-from statcast.better.sm import BetterGLM, BetterMNLogit
-from statcast.better.spark import cross_val_predict
 from statcast.tools.plot import plotPrecRec, plotPrecRecMN, plotResiduals
+from statcast.better.declassifier import KDEClassifier
 
 
 # %%
@@ -39,77 +36,80 @@ for out in outs:
 
 subData['events'] = subData['events'].cat.remove_unused_categories()
 
-X1 = subData.loc[:, xLabels[:-1]]
-X2 = subData.loc[:, xLabels]
-y1 = subData[yLabel] == 'Home Run'
-y2 = subData[yLabel]
+X1 = subData.loc[:, xLabels[:-1]].values
+X2 = subData.loc[:, xLabels].values
+y1 = (subData[yLabel] == 'Home Run').values
+y2 = subData[yLabel].values
 
 skf = StratifiedKFold(n_splits=10, shuffle=True)
+test, train = next(skf.split(X1, y1))
 
-glm = BetterGLM(
-    SMParams={'family': sm.families.Binomial(sm.families.links.probit)})
-mnLogit = BetterMNLogit()
+kdc = KDEClassifier(kdeParams=dict(kernel='gaussian'),
+                    n_jobs=-1)
+est11 = clone(kdc).fit(X1[train], y1[train])
+est12 = clone(kdc).fit(X1[train], y2[train])
+est21 = clone(kdc).fit(X2[train], y1[train])
+est22 = clone(kdc).fit(X2[train], y2[train])
 
-y11p = cross_val_predict(glm, X1, y1, cv=skf, n_jobs=-1,
-                         method='predict_proba')
-y21p = cross_val_predict(glm, X2, y1, cv=skf, n_jobs=-1,
-                         method='predict_proba')
-y12p = cross_val_predict(mnLogit, X1, y2, cv=skf, n_jobs=-1,
-                         method='predict_proba')
-y22p = cross_val_predict(mnLogit, X2, y2, cv=skf, n_jobs=-1,
-                         method='predict_proba')
+y11p = est11.predict_proba(X1[test])
+y12p = est12.predict_proba(X1[test])
+y21p = est21.predict_proba(X2[test])
+y22p = est22.predict_proba(X2[test])
+
+y11p = y11p[:, 1]
+y21p = y21p[:, 1]
 
 # %% Log-loss
 
-L11 = np.exp(-log_loss(y1, y11p))
-L21 = np.exp(-log_loss(y1, y21p))
-L12 = np.exp(-log_loss(y2, y12p))
-L22 = np.exp(-log_loss(y2, y22p))
+logL11 = log_loss(y1[test], y11p)
+logL21 = log_loss(y1[test], y21p)
+logL12 = log_loss(y2[test], y12p)
+logL22 = log_loss(y2[test], y22p)
 
 # %% Plot Precision-Recall Curve
 
-fig = plotPrecRec(y1, y11p, label='EV + LA: L={:.0f}%'.format(L11 * 100))
+fig = plotPrecRec(y1[test], y11p, label='EV + LA: LL={:.2f}'.format(logL11))
 ax = fig.gca()
-plotPrecRec(y1, y21p, ax=ax, label='EV + LA + SA: L={:.0f}%'.format(L21 * 100))
+plotPrecRec(y1[test], y21p, ax=ax, label='EV + LA + SA: LL={:.2f}'.format(logL21))
 ax.legend()
-ax.set_title('GLM Homerun Classifier')
-fig.savefig('GLM HR Prec-Rec Curve')
+ax.set_title('KDC Homerun Classifier')
+#fig.savefig('KDC HR Prec-Rec Curve')
 
-fig = plotPrecRecMN(y2, y12p)
-fig.gca().set_title('EV + LA: L={:.0f}%'.format(L12 * 100))
-fig.savefig('GLM(EV + LA) Hit Prec-Rec Curves')
-fig = plotPrecRecMN(y2, y22p)
-fig.gca().set_title('EV + LA + SA: L={:.0f}%'.format(L22 * 100))
-fig.savefig('GLM(EV + LA + SA) Hit Prec-Rec Curves')
+fig = plotPrecRecMN(y2[test], y12p)
+fig.gca().set_title('EV + LA: LL={:.2f}'.format(logL12))
+#fig.savefig('KDC(EV + LA) Hit Prec-Rec Curves')
+fig = plotPrecRecMN(y2[test], y22p)
+fig.gca().set_title('EV + LA + SA: LL={:.2f}'.format(logL22))
+#fig.savefig('KDC(EV + LA + SA) Hit Prec-Rec Curves')
 
 # %% Plot Residuals
 
-figs11 = plotResiduals(X1.values, y1 * 100, y11p * 100,
+figs11 = plotResiduals(X1[test], y1[test] * 100, y11p * 100,
                        xLabels=fancyLabels[:-1], xUnits=units[:-1],
                        yLabels=['Home Run'], yUnits=['%'], pltParams={'ms': 1})
 for label, fig in zip(xLabels[:-1], figs11):
     fig.gca().set_title('HR(EV + LA) Classifier')
-    fig.savefig('GLM(EV + LA) HR Residuals over {}'.format(label))
-figs21 = plotResiduals(X2.values, y1 * 100, y21p * 100,
+#    fig.savefig('KDC(EV + LA) HR Residuals over {}'.format(label))
+figs21 = plotResiduals(X2[test], y1[test] * 100, y21p * 100,
                        xLabels=fancyLabels, xUnits=units,
                        yLabels=['Home Run'], yUnits=['%'], pltParams={'ms': 1})
 for label, fig in zip(xLabels, figs21):
     fig.gca().set_title('HR(EV + LA + SA) Classifier')
-    fig.savefig('GLM(EV + LA + SA) HR Residuals over {}'.format(label))
+#    fig.savefig('KDC(EV + LA + SA) HR Residuals over {}'.format(label))
 
 Y2 = LabelBinarizer().fit_transform(y2)
 
-figs12 = plotResiduals(X1.values, Y2 * 100, y12p * 100,
+figs12 = plotResiduals(X1[test], Y2[test] * 100, y12p * 100,
                        xLabels=fancyLabels[:-1], xUnits=units[:-1],
-                       yLabels=y2.cat.categories, yUnits=['%'] * Y2.shape[1],
+                       yLabels=est12.classes_, yUnits=['%'] * Y2.shape[1],
                        pltParams={'ms': 1})
 for label, fig in zip(xLabels[:-1], figs12):
     fig.get_axes()[0].set_title('Hit(EV + LA) Classifier')
-    fig.savefig('GLM(EV + LA) Hit Residuals over {}'.format(label))
-figs22 = plotResiduals(X2.values, Y2 * 100, y22p * 100,
+#    fig.savefig('KDC(EV + LA) Hit Residuals over {}'.format(label))
+figs22 = plotResiduals(X2[test], Y2[test] * 100, y22p * 100,
                        xLabels=fancyLabels, xUnits=units,
-                       yLabels=y2.cat.categories, yUnits=['%'] * Y2.shape[1],
+                       yLabels=est22.classes_, yUnits=['%'] * Y2.shape[1],
                        pltParams={'ms': 1})
 for label, fig in zip(xLabels, figs22):
     fig.get_axes()[0].set_title('Hit(EV + LA + SA) Classifier')
-    fig.savefig('GLM(EV + LA + SA) Hit Residuals over {}'.format(label))
+#    fig.savefig('KDC(EV + LA + SA) Hit Residuals over {}'.format(label))
